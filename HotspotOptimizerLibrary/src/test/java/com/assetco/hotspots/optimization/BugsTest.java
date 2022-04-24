4d4ac34856578b640ce6fd34ec9912006028c60b
lrichard <laurent-richard@harvest.Fr>
2022-04-24 16:03:55 +0200

    first bug test implemented (green for actual bugged code)

--- HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
+++ HotspotOptimizerLibrary/src/test/java/com/assetco/hotspots/optimization/BugsTest.java
@@ -1,6 +1,7 @@
 package com.assetco.hotspots.optimization;
 
 import com.assetco.search.results.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,8 +9,19 @@ import java.util.List;
 
 import static com.assetco.search.results.AssetVendorRelationshipLevel.*;
 import static com.assetco.search.results.HotspotKey.Showcase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class BugsTest {
+    private SearchResults results;
+    private SearchResultHotspotOptimizer optimizer;
+
+    @BeforeEach
+    public void setup() {
+        results = new SearchResults();
+        optimizer = new SearchResultHotspotOptimizer();
+    }
+
     @Test
     void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
         AssetVendor partnerVendor = makeVendor(Partner);
@@ -28,18 +40,40 @@ public class BugsTest {
     }
 
     private AssetVendor makeVendor(AssetVendorRelationshipLevel relationshipLevel) {
-        return null;
+        var id = Any.string();
+        var displayName = Any.string();
+        long royaltyRate = Any.anyLong();
+
+        var vendor = new AssetVendor(id, displayName, relationshipLevel, royaltyRate);
+
+        return vendor;
     }
 
     private Asset givenAssetInResultsWithVendor(AssetVendor vendor) {
-        return null;
+        var id = Any.string();
+        var title = Any.string();
+        var thumbnailURI = Any.URI();
+        var previewURI = Any.URI();
+        var last30Days = Any.assetPurchaseInfo();
+        var last24Hours = Any.assetPurchaseInfo();
+        var topics = Any.setOfTopics();
+
+        var asset = new Asset(id, title, thumbnailURI, previewURI, last30Days, last24Hours, topics, vendor);
+        results.addFound(asset);
+
+        return asset;
     }
 
     private void whenOptimize() {
+        optimizer.optimize(results);
     }
     private void thenHotspotDoesNotHave(HotspotKey key, Asset... assets) {
+        for (Asset asset : assets) {
+            assertFalse(results.getHotspot(key).getMembers().contains(asset));
+        }
     }
 
     private void thenHotspotHasExactly(HotspotKey key, List<Asset> assets) {
+        assertEquals(assets, results.getHotspot(key).getMembers());
     }
 }
